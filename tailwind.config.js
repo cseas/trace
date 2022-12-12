ba548c4379d07d1b7798cd233614120de697317e
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-12-12 22:35:31 +0530

    fix: add missing import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const radixColors = require("@radix-ui/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
