6f3b040252b703e9a9ce599a268a4d7f76798ac8
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-12-12 22:25:36 +0530

    feat: add radix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("windy-radix-palette")],
 };
