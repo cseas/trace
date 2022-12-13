dc76886dbe8660639e39516bef555228903d672e
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-12-13 20:42:24 +0530

    feat: add api call for services metadata (#4)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../src/components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </Layout>
   );
 }
--- pages/services.tsx
+++ pages/services.tsx
@@ -1,7 +1,48 @@
+import { request, gql } from "graphql-request";
+import { useQuery } from "@tanstack/react-query";
+
+interface ServicesQuery {
+  metadata: {
+    name: string;
+    displayName: string;
+    type: string;
+    scope: string;
+    units: string;
+    onlySupportsAggregation: boolean;
+    onlySupportsGrouping: boolean;
+    supportedAggregations: string[];
+    groupable: boolean;
+    __typename: string;
+  }[];
+}
+
+const servicesQuery = gql`
+  {
+    metadata {
+      name
+      displayName
+      type
+      scope
+      units
+      onlySupportsAggregation
+      onlySupportsGrouping
+      supportedAggregations
+      groupable
+      __typename
+    }
+  }
+`;
+
 export default function Services() {
+  const { data } = useQuery<ServicesQuery>(["services"], async () => {
+    const { metadata } = await request(
+      process.env.NEXT_PUBLIC_GRAPHQL!,
+      servicesQuery
+    );
+    return metadata;
+  });
+
   return (
-    <div className="text-3xl text-mauve-12">
-      services {process.env.NEXT_PUBLIC_GRAPHQL}
-    </div>
+    <pre className="text-sm text-mauve-12">{JSON.stringify(data, null, 4)}</pre>
   );
 }
